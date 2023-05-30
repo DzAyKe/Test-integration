d3b0480c365f9f0dc4e241b0d4e0d0bc375e37f5
root <root@B3Infra4-jenkins.8.8.8.8>
2023-05-30 08:41:45 +0000

    ezf

--- test_suite.sh
+++ test_suite.sh
@@ -3,11 +3,13 @@
 # ===============
 # Test le script suite avec les valeurs 1, 2, 3 et vérifier que le résultat est bien 5
 echo "test unitaire du script suite.sh"
-resultat=$(/var/script/suite.sh 1 2 3)
+resultat=$(./script.sh 1 2 3)
 
 if [[ $resultat -eq 5 ]]
 then
+	echo "egale à 5"
     exit 0
 else
+	echo "pas égale à 5"
     exit 1
 fi
